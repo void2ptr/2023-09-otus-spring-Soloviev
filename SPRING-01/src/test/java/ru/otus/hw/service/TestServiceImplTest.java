682d4e373fe16c73aa9ed3ea4cc11eb7fc5e1e51
Alexander Soloviev <a.a.so@yandex.ru>
2023-10-18 11:31:29 +0300

    [SPRING-01]
    - links:
    ref-jira: [SPRING-01]

--- SPRING-01/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ SPRING-01/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -5,49 +5,45 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw.config.AppConfig;
 import ru.otus.hw.dao.CsvQuestionDao;
-import ru.otus.hw.domain.Answer;
-import ru.otus.hw.domain.Question;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @DisplayName("Service TestServiceImpl")
 class TestServiceImplTest {
-    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private static final StreamsIOService stream = new StreamsIOService(new PrintStream(outContent));
-    private AppConfig stubAppConfig = mock(AppConfig.class);
-    private CsvQuestionDao stubCsvQuestionDao = mock(CsvQuestionDao.class);
-    private TestServiceImpl service = new TestServiceImpl(stream, stubCsvQuestionDao);
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private StreamsIOService spyStream = mock(StreamsIOService.class, withSettings()
+            .useConstructor(new PrintStream(outContent))
+            .defaultAnswer(CALLS_REAL_METHODS)
+    );
+    private AppConfig mockAppConfig = mock(AppConfig.class);
+    private CsvQuestionDao csvQuestionDao;
+    private TestServiceImpl service;
 
-    @DisplayName("Init the test")
+    @DisplayName("Read [resource:questions_test.csv]")
     @BeforeEach
     void setUp() {
-        // init
-        List<Answer> answers = new ArrayList<>();
-        answers.add(new Answer("Answer 1 ?", false));
-        answers.add(new Answer("Answer 2 ?", true));
-        answers.add(new Answer("Answer 3 ?", false));
-        Question question = new Question("Question 1", answers);
-        List<Question> questions = new ArrayList<>();
-        questions.add(question);
         // mock
-        when(stubAppConfig.getTestFileName()).thenReturn("questions_test.csv");
-        when(stubCsvQuestionDao.findAll()).thenReturn(questions);
+        when(mockAppConfig.getTestFileName()).thenReturn("questions_test.csv");
+        //
+        csvQuestionDao = new CsvQuestionDao(mockAppConfig);
+        service = new TestServiceImpl(spyStream, csvQuestionDao);
     }
 
-    @DisplayName("The questions contain the words")
+    @DisplayName("he questions [resource:file] contain the words")
     @Test
     void executeTest() {
         // tested method
         service.executeTest();
         // test
         String expected = outContent.toString();
-        assertTrue( expected.contains("Question 1"), "is contain [Question 1] ?");
+        assertTrue( expected.contains("Question 1"), "contain [Question 1]");
+        assertTrue( expected.contains("Question 2"), "contain [Question 2]");
+        assertTrue( expected.contains("Question 3"), "contain [Question 3]");
+        assertTrue( expected.contains("Question 4"), "contain [Question 4]");
+        assertTrue( expected.contains("Question 5"), "contain [Question 5]");
     }
 }
