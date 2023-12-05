158ba69f107574f4a2428ec92773ebdfae70b930
Alexander Soloviev <a.a.so@yandex.ru>
2023-12-05 21:13:41 +0300

    [SPRING-08]
    - links:
    ref-jira: [SPRING-08]

--- SPRING-08/src/main/java/ru/otus/hw/services/BookService.java
+++ SPRING-08/src/main/java/ru/otus/hw/services/BookService.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.services;
 
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
@@ -8,18 +7,13 @@ import java.util.Optional;
 
 public interface BookService {
 
-    @Transactional(readOnly = true)
     Optional<Book> findById(String id);
 
-    @Transactional(readOnly = true)
     List<Book> findAll();
 
-    @Transactional
     Book insert(String title, List<String> authorNames, List<String> genresIds);
 
-    @Transactional
     Book update(String title, List<String> authorNames, List<String> genresIds);
 
-    @Transactional
     void deleteByTitle(String title);
 }
--- SPRING-08/src/main/java/ru/otus/hw/services/CommentService.java
+++ SPRING-08/src/main/java/ru/otus/hw/services/CommentService.java
@@ -1,28 +1,21 @@
 package ru.otus.hw.services;
 
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
 public interface CommentService {
 
-    @Transactional(readOnly = true)
     List<Comment> findCommentsByBookTitle(String title);
 
-    @Transactional
     Comment insert(String bookId);
 
-    @Transactional
     void delete(String commentId);
 
-    @Transactional
     Comment insertNote(String title, String note);
 
-    @Transactional
     Comment updateNote(String title, int commentId, String note);
 
-    @Transactional
     Comment deleteNote(String title, int noteId);
 
 }
