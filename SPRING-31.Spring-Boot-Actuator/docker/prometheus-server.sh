68c8e81c68483b3a4eea38ed410888c99453a8c1
Alexander Soloviev <a.a.so@yandex.ru>
2024-02-11 20:36:27 +0300

    [SPRING-31]

---
+++ SPRING-31.Spring-Boot-Actuator/docker/prometheus-server.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd ..
+root=`pwd`
+
+echo  $root/target/classes/prometheus.yml
+
+# Create persistent volume for your data
+docker volume create prometheus-data
+
+## Start Prometheus container
+docker run -td -p 9095:9090 --name prometheus1 -v prometheus-data:/prometheus prom/prometheus
+
+#docker run \
+#    -p 9095:9090 \
+#    -v $root/target/classes/prometheus.yml:/etc/prometheus/prometheus.yml \
+#    prom/prometheus
+
---
+++ SPRING-31.Spring-Boot-Actuator/src/main/java/ru/otus/hw/actuators/AuthorHealthIndicator.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.actuators;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.model.Author;
+import ru.otus.hw.repository.AuthorRepository;
+
+import java.security.SecureRandom;
+import java.util.Optional;
+
+
+@Component
+public class AuthorHealthIndicator implements HealthIndicator {
+    private final SecureRandom random = new SecureRandom();
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorHealthIndicator(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public Health health() {
+        long count = authorRepository.count();
+        long randAuthorId = random.nextLong(count);
+        Optional<Author> authorOpt = authorRepository.findAuthorById(randAuthorId);
+        if (authorOpt.isPresent()) {
+            return Health.down()
+                    .status(Status.UP)
+                    .withDetail("author", authorOpt.get().getFullName())
+                    .build();
+        }
+        return Health.down()
+                .status(Status.DOWN)
+                .withDetail("author", "all authors dead %d".formatted(randAuthorId))
+                .build();
+    }
+}
